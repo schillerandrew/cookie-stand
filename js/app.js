e332a110cf924390bd4addb022df3bd68e60a6d9
schillerandrew <hyogoetophile@gmail.com>
2022-03-15 15:57:43 -0700

    refactored code using constructor, table content is good

--- js/app.js
+++ js/app.js
@@ -1,362 +1,116 @@
+// generates a random number, given a min and max number
 function randomNumber(min, max) {
-  // (next line) grabbed from MDN documentation
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  // (next line) was grabbed from MDN documentation
+  return Math.random() * (max - min + 1) + min;
+}
+// create window into DOM
+let tableElem = document.getElementById('city-schedule');
+
+// holds values from Store objects
+let storeValues = [];
+
+// object constructor for making stores
+function Store(city, minCust, maxCust, avgCook) {
+  this.cityName = city;
+  this.minCustomersPerHour = minCust;
+  this.maxCustomersPerHour = maxCust;
+  this.avgCookiesPerSale = avgCook;
+  this.cookiesForGivenHour = [];
+  this.totalCookies = 0;
+
+  storeValues.push(this);
 }
 
-let cityProfile = document.getElementById('city-profile');
-
-// Seattle object <><><><><><><><><><><><><><><><><><><><><><>
-let seattle = {
-  cityName: 'Seattle',
-  // minimum number of customers each hour
-  minCustomersPerHour: 23,
-  // maximum number of customers each hour
-  maxCustomersPerHour: 65,
-  // average number of cookies purchased in each sale
-  avgCookiesPerSale: 6.3,
-  // hold current values for cookies needed at hour intervals in a day
-  cookiesForGivenHour: [],
-  // hold current value for total cookies needed for a day
-  totalCookies: 0,
-  // generate random number of customers for an hour slot, given min and max
-  getRandomCustomers: function () {
-    return randomNumber(this.minCustomersPerHour, this.maxCustomersPerHour);
-  },
-  // calculate whole-number of cookies needed for an hour interval, for entire day, and tallies day total
-  getCookieSchedule: function () {
-    let i = 0;
-    for (i = 0; i < 14; i++) {
-      this.cookiesForGivenHour[i] = Math.floor(this.getRandomCustomers() * this.avgCookiesPerSale);
-      this.totalCookies = this.totalCookies + Math.floor(this.getRandomCustomers() * this.avgCookiesPerSale);
-    }
-  },
-  // render content to HTML page
-  renderSchedule: function () {
-    // create h2 element
-    let h2Tag = document.createElement('h2');
-    // add context
-    h2Tag.innerText = `${this.cityName}`;
-    // add to DOM
-    cityProfile.appendChild(h2Tag);
-
-    // create ul element
-    let ulTag = document.createElement('ul');
-    // add to DOM
-    cityProfile.appendChild(ulTag);
-
-    // create hour labels
-    let hourOfTheDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-    // create li elements
-    let i = 0;
-    for (i = 0; i < 14; i++) {
-      let liTag = document.createElement('li');
-      // add context
-      liTag.innerText = `${hourOfTheDay[i]}: ${this.cookiesForGivenHour[i]} cookies`;
-      // add to DOM
-      cityProfile.appendChild(liTag);
-    }
-    // create "Total" li element
-    let liTag = document.createElement('li');
-    // add context
-    liTag.innerText = `Total: ${this.totalCookies} cookies`;
-    // add to DOM
-    cityProfile.appendChild(liTag);
-
-    // create line break
-    let brTag = document.createElement('br');
-    // add to DOM
-    cityProfile.appendChild(brTag);
+// method to calculate cookies needed for each hour, given customer guesstimate
+Store.prototype.calculateCookieSchedule = function () {
+  let i = 0;
+  for (i = 0; i < 14; i++) {
+    this.cookiesForGivenHour[i] = Math.floor(randomNumber(this.minCustomersPerHour, this.maxCustomersPerHour) * this.avgCookiesPerSale);
+    this.totalCookies = this.totalCookies + Math.floor(randomNumber(this.minCustomersPerHour, this.maxCustomersPerHour) * this.avgCookiesPerSale);
   }
 };
 
-// generate random number of customers
-seattle.getRandomCustomers();
-// generate schedule of estimated customers and needed cookies each hour for day
-seattle.getCookieSchedule();
-// render the schedule to sales.html in unordered list
-seattle.renderSchedule();
+// method to render header row to HTML page
+Store.prototype.renderHeader = function () {
+  // create a header row
+  let headerRow = document.createElement('tr');
+  // add to DOM
+  tableElem.appendChild(headerRow);
 
-// Tokyo object <><><><><><><><><><><><><><><><><><><><><><>
-let tokyo = {
-  cityName: 'Tokyo',
-  // minimum number of customers each hour
-  minCustomersPerHour: 3,
-  // maximum number of customers each hour
-  maxCustomersPerHour: 24,
-  // average number of cookies purchased in each sale
-  avgCookiesPerSale: 1.2,
-  // hold current values for cookies needed at hour intervals in a day
-  cookiesForGivenHour: [],
-  // hold current value for total cookies needed for a day
-  totalCookies: 0,
-  // generate random number of customers for an hour slot, given min and max
-  getRandomCustomers: function () {
-    return randomNumber(this.minCustomersPerHour, this.maxCustomersPerHour);
-  },
-  // calculate whole-number of cookies needed for an hour interval, for entire day, and tallies day total
-  getCookieSchedule: function () {
-    let i = 0;
-    for (i = 0; i < 14; i++) {
-      this.cookiesForGivenHour[i] = Math.floor(this.getRandomCustomers() * this.avgCookiesPerSale);
-      this.totalCookies = this.totalCookies + Math.floor(this.getRandomCustomers() * this.avgCookiesPerSale);
-    }
-  },
-  // render content to HTML page
-  renderSchedule: function () {
-    // create h2 element
-    let h2Tag = document.createElement('h2');
+  // create empty top-left cell
+  let topLeftEmptyCell = document.createElement('th');
+  // no context needed
+  // add to DOM
+  headerRow.appendChild(topLeftEmptyCell);
+
+  // array for holding each header
+  let hourOfTheDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+  // render each header
+  let i = 0;
+  for (i = 0; i < hourOfTheDay.length; i++) {
+    // create header
+    let headers = document.createElement('th');
     // add context
-    h2Tag.innerText = `${this.cityName}`;
+    headers.innerText = `${hourOfTheDay[i]}`;
     // add to DOM
-    cityProfile.appendChild(h2Tag);
-
-    // create ul element
-    let ulTag = document.createElement('ul');
-    // add to DOM
-    cityProfile.appendChild(ulTag);
-
-    // create hour labels
-    let hourOfTheDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-    // create li elements
-    let i = 0;
-    for (i = 0; i < 14; i++) {
-      let liTag = document.createElement('li');
-      // add context
-      liTag.innerText = `${hourOfTheDay[i]}: ${this.cookiesForGivenHour[i]} cookies`;
-      // add to DOM
-      cityProfile.appendChild(liTag);
-    }
-    // create "Total" li element
-    let liTag = document.createElement('li');
-    // add context
-    liTag.innerText = `Total: ${this.totalCookies} cookies`;
-    // add to DOM
-    cityProfile.appendChild(liTag);
-
-    // create line break
-    let brTag = document.createElement('br');
-    // add to DOM
-    cityProfile.appendChild(brTag);
+    headerRow.appendChild(headers);
   }
+
+  // create header for "Daily Location Total"
+  let headerDailyTotal = document.createElement('th');
+  // add context
+  headerDailyTotal.innerText = 'Daily Location Total';
+  // add to DOM
+  headerRow.appendChild(headerDailyTotal);
 };
 
-// generate random number of customers
-tokyo.getRandomCustomers();
-// generate schedule of estimated customers and needed cookies each hour for day
-tokyo.getCookieSchedule();
-// render the schedule to sales.html in unordered list
-tokyo.renderSchedule();
+// method for rendering one row to the table, that contains cookie guesstimates
+Store.prototype.renderStore = function() {
 
+  // create a row
+  let row = document.createElement('tr');
+  // add to DOM
+  tableElem.appendChild(row);
 
-// Dubai object <><><><><><><><><><><><><><><><><><><><><><>
-let dubai = {
-  cityName: 'Dubai',
-  // minimum number of customers each hour
-  minCustomersPerHour: 11,
-  // maximum number of customers each hour
-  maxCustomersPerHour: 38,
-  // average number of cookies purchased in each sale
-  avgCookiesPerSale: 3.7,
-  // hold current values for cookies needed at hour intervals in a day
-  cookiesForGivenHour: [],
-  // hold current value for total cookies needed for a day
-  totalCookies: 0,
-  // generate random number of customers for an hour slot, given min and max
-  getRandomCustomers: function () {
-    return randomNumber(this.minCustomersPerHour, this.maxCustomersPerHour);
-  },
-  // calculate whole-number of cookies needed for an hour interval, for entire day, and tallies day total
-  getCookieSchedule: function () {
-    let i = 0;
-    for (i = 0; i < 14; i++) {
-      this.cookiesForGivenHour[i] = Math.floor(this.getRandomCustomers() * this.avgCookiesPerSale);
-      this.totalCookies = this.totalCookies + Math.floor(this.getRandomCustomers() * this.avgCookiesPerSale);
-    }
-  },
-  // render content to HTML page
-  renderSchedule: function () {
-    // create h2 element
-    let h2Tag = document.createElement('h2');
+  // create td for city name
+  let tdName = document.createElement('td');
+  // add context
+  tdName.innerText = `${this.cityName}`;
+  // add to DOM
+  row.appendChild(tdName);
+
+  // render each td, which contains a cookie guesstimate
+  let i = 0;
+  for (i = 0; i < this.cookiesForGivenHour.length; i++) {
+    // create td
+    let tdTag = document.createElement('td');
     // add context
-    h2Tag.innerText = `${this.cityName}`;
+    tdTag.innerText = `${this.cookiesForGivenHour[i]}`;
     // add to DOM
-    cityProfile.appendChild(h2Tag);
-
-    // create ul element
-    let ulTag = document.createElement('ul');
-    // add to DOM
-    cityProfile.appendChild(ulTag);
-
-    // create hour labels
-    let hourOfTheDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-    // create li elements
-    let i = 0;
-    for (i = 0; i < 14; i++) {
-      let liTag = document.createElement('li');
-      // add context
-      liTag.innerText = `${hourOfTheDay[i]}: ${this.cookiesForGivenHour[i]} cookies`;
-      // add to DOM
-      cityProfile.appendChild(liTag);
-    }
-    // create "Total" li element
-    let liTag = document.createElement('li');
-    // add context
-    liTag.innerText = `Total: ${this.totalCookies} cookies`;
-    // add to DOM
-    cityProfile.appendChild(liTag);
-
-    // create line break
-    let brTag = document.createElement('br');
-    // add to DOM
-    cityProfile.appendChild(brTag);
+    row.appendChild(tdTag);
   }
+
+  // create td, for daily cookie total
+  let tdTagTotal = document.createElement('td');
+  // add context
+  tdTagTotal.innerText = `${this.totalCookies}`;
+  // add to DOM
+  row.appendChild(tdTagTotal);
 };
 
-// generate random number of customers
-dubai.getRandomCustomers();
-// generate schedule of estimated customers and needed cookies each hour for day
-dubai.getCookieSchedule();
-// render the schedule to sales.html in unordered list
-dubai.renderSchedule();
+// creating an object for each store
+new Store ('Seattle', 23, 65, 6.3);
+new Store ('Tokyo', 3, 24, 1.2);
+new Store ('Dubai', 11, 38, 3.7);
+new Store ('Paris', 20, 38, 2.3);
+new Store ('Lima', 2, 16, 4.6);
 
-// Paris object <><><><><><><><><><><><><><><><><><><><><><>
-let paris = {
-  cityName: 'Paris',
-  // minimum number of customers each hour
-  minCustomersPerHour: 20,
-  // maximum number of customers each hour
-  maxCustomersPerHour: 38,
-  // average number of cookies purchased in each sale
-  avgCookiesPerSale: 2.3,
-  // hold current values for cookies needed at hour intervals in a day
-  cookiesForGivenHour: [],
-  // hold current value for total cookies needed for a day
-  totalCookies: 0,
-  // generate random number of customers for an hour slot, given min and max
-  getRandomCustomers: function () {
-    return randomNumber(this.minCustomersPerHour, this.maxCustomersPerHour);
-  },
-  // calculate whole-number of cookies needed for an hour interval, for entire day, and tallies day total
-  getCookieSchedule: function () {
-    let i = 0;
-    for (i = 0; i < 14; i++) {
-      this.cookiesForGivenHour[i] = Math.floor(this.getRandomCustomers() * this.avgCookiesPerSale);
-      this.totalCookies = this.totalCookies + Math.floor(this.getRandomCustomers() * this.avgCookiesPerSale);
-    }
-  },
-  // render content to HTML page
-  renderSchedule: function () {
-    // create h2 element
-    let h2Tag = document.createElement('h2');
-    // add context
-    h2Tag.innerText = `${this.cityName}`;
-    // add to DOM
-    cityProfile.appendChild(h2Tag);
-
-    // create ul element
-    let ulTag = document.createElement('ul');
-    // add to DOM
-    cityProfile.appendChild(ulTag);
-
-    // create hour labels
-    let hourOfTheDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-    // create li elements
-    let i = 0;
-    for (i = 0; i < 14; i++) {
-      let liTag = document.createElement('li');
-      // add context
-      liTag.innerText = `${hourOfTheDay[i]}: ${this.cookiesForGivenHour[i]} cookies`;
-      // add to DOM
-      cityProfile.appendChild(liTag);
-    }
-    // create "Total" li element
-    let liTag = document.createElement('li');
-    // add context
-    liTag.innerText = `Total: ${this.totalCookies} cookies`;
-    // add to DOM
-    cityProfile.appendChild(liTag);
-
-    // create line break
-    let brTag = document.createElement('br');
-    // add to DOM
-    cityProfile.appendChild(brTag);
+function displayStores() {
+  for (let i = 0; i < storeValues.length; i++) {
+    storeValues[i].calculateCookieSchedule();
+    storeValues[i].renderHeader();
+    storeValues[i].renderStore();
   }
-};
+}
 
-// generate random number of customers
-paris.getRandomCustomers();
-// generate schedule of estimated customers and needed cookies each hour for day
-paris.getCookieSchedule();
-// render the schedule to sales.html in unordered list
-paris.renderSchedule();
-
-// Lima object <><><><><><><><><><><><><><><><><><><><><><>
-let lima = {
-  cityName: 'Lima',
-  // minimum number of customers each hour
-  minCustomersPerHour: 2,
-  // maximum number of customers each hour
-  maxCustomersPerHour: 16,
-  // average number of cookies purchased in each sale
-  avgCookiesPerSale: 4.6,
-  // hold current values for cookies needed at hour intervals in a day
-  cookiesForGivenHour: [],
-  // hold current value for total cookies needed for a day
-  totalCookies: 0,
-  // generate random number of customers for an hour slot, given min and max
-  getRandomCustomers: function () {
-    return randomNumber(this.minCustomersPerHour, this.maxCustomersPerHour);
-  },
-  // calculate whole-number of cookies needed for an hour interval, for entire day, and tallies day total
-  getCookieSchedule: function () {
-    let i = 0;
-    for (i = 0; i < 14; i++) {
-      this.cookiesForGivenHour[i] = Math.floor(this.getRandomCustomers() * this.avgCookiesPerSale);
-      this.totalCookies = this.totalCookies + Math.floor(this.getRandomCustomers() * this.avgCookiesPerSale);
-    }
-  },
-  // render content to HTML page
-  renderSchedule: function () {
-    // create h2 element
-    let h2Tag = document.createElement('h2');
-    // add context
-    h2Tag.innerText = `${this.cityName}`;
-    // add to DOM
-    cityProfile.appendChild(h2Tag);
-
-    // create ul element
-    let ulTag = document.createElement('ul');
-    // add to DOM
-    cityProfile.appendChild(ulTag);
-
-    // create hour labels
-    let hourOfTheDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-    // create li elements
-    let i = 0;
-    for (i = 0; i < 14; i++) {
-      let liTag = document.createElement('li');
-      // add context
-      liTag.innerText = `${hourOfTheDay[i]}: ${this.cookiesForGivenHour[i]} cookies`;
-      // add to DOM
-      cityProfile.appendChild(liTag);
-    }
-    // create "Total" li element
-    let liTag = document.createElement('li');
-    // add context
-    liTag.innerText = `Total: ${this.totalCookies} cookies`;
-    // add to DOM
-    cityProfile.appendChild(liTag);
-
-    // create line break
-    let brTag = document.createElement('br');
-    // add to DOM
-    cityProfile.appendChild(brTag);
-  }
-};
-
-// generate random number of customers
-lima.getRandomCustomers();
-// generate schedule of estimated customers and needed cookies each hour for day
-lima.getCookieSchedule();
-// render the schedule to sales.html in unordered list
-lima.renderSchedule();
+displayStores();
